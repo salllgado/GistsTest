bf4401d9e1190b1a379b59e8ad577374a5a6a0fc
Chrystian Salgado <chrystian.salgado@madeinweb.app>
2024-05-10 14:01:07 -0300

    feat: setuping project

---
+++ GistsTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GistsTests
+//
+//  Created by Chrystian Salgado on 10/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
